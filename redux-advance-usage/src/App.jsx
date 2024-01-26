a63ff46f99c9b69a881886c90bcaef946ff47a02
Medetcan Diler <medetdiler1996@gmail.com>
2024-01-26 21:20:07 +0300

    Deleted unused component

--- redux-advance-usage/src/App.jsx
+++ redux-advance-usage/src/App.jsx
@@ -1,7 +1,6 @@
 import './index.css'
 import PostsList from "./app/features/post/PostLists";
 import PostForm from "./app/features/post/PostForm";
-import UserLists from './app/features/user/UserLists';
 
 function App() {
   return (
